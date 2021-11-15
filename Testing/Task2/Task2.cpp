dd741fa4f169627e8e93b0b6ba37bf5186393fc9
UnicornMane <isaenkovcc@mail.ru>
2021-11-15 13:24:43 +0300

    uploading my testing 15.11.2021

---
+++ Testing/Task2/Task2.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    bool even = false;
+    int mini = 10001;
+    int maxi = -10001;
+    {
+        int t = 0;
+        while (cin >> t)
+        {
+            if (even)
+            {
+                maxi = max(t, maxi);
+            }
+            else
+            {
+                mini = min(t, mini);
+            }
+            even = !even;
+        }
+    }
+    cout << maxi + mini;
+    return EXIT_SUCCESS;
+}
